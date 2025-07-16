d246f0568c9019cee4f7b0171a3faeb4d13748f3
Alex Joseph <alexjoseph@mac.mynetworksettings.com>
2025-07-16 09:50:15 -0400

    Hotfix

--- delete_s3.py
+++ delete_s3.py
@@ -1,18 +1,18 @@
 import os
 import sys
 PATH= os.environ.get('PATH', '')
+BUCKET_NAME = os.environ.get('BUCKET_LOGICAL_NAME', '')
+
 sys.path.append(PATH)
 import boto3
 
 
-BUCKET_NAME = os.environ.get('BUCKET_LOGICAL_NAME', '')
-BUCKET =  BUCKET_NAME
 
 s3 = boto3.resource('s3')
-bucket = s3.Bucket(BUCKET)
-print(f"Deleting all objects in bucket: {BUCKET}")
+bucket = s3.Bucket(BUCKET_NAME)
+print(f"Deleting all objects in bucket: {BUCKET_NAME}")
 bucket.object_versions.delete()
 
 # if you want to delete the now-empty bucket as well, uncomment this line:
-print(f"Deleting bucket: {BUCKET}")
+print(f"Deleting bucket: {BUCKET_NAME}")
 bucket.delete()
